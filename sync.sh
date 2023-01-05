28d8acdc7291b7624860fb8f46090ff6b78f8943
Kirill <kirill@shoniya.net>
2023-01-06 01:07:29 +0300

    Logging improvements and refactoring
    1. Add logger function
    2. Add Telegram API status code check

--- sync.sh
+++ sync.sh
@@ -17,16 +17,41 @@ DATE_TIME_NOW=$(date +'%Y-%m-%d %H:%M:%S');
 DEFAULT_COMPRESSION_LEVEL=9;
 is_telegram_available=0;
 
+function logger() {
+    printf "[$(date +'%Y-%m-%d %H:%M:%S:%3N')] - $1 - $2\n";
+}
+
 function notify_telegram {
+  logger "INFO" "Sending request to Telegram API";
   curl -s -X POST \
    -H 'Content-Type: application/json' \
    -d "{\"chat_id\": \"$2\", \"text\": \"$3\", \"disable_notification\": true}" \
    https://api.telegram.org/bot$1/sendMessage \
    > /dev/null
+
+   status_code=$(
+    curl --write-out %{http_code} -s -X POST -H 'Content-Type: application/json' \
+       -d "{\"chat_id\": \"$2\", \"text\": \"$3\", \"disable_notification\": true}" \
+       https://api.telegram.org/bot$1/sendMessage \
+       > /dev/null
+    );
+
+   if [[ "$status_code" != 200 ]] ; then
+     logger "CRITICAL" "Telegram API status code is not 200";
+     return 1;
+   else
+     return 0;
+   fi
 }
 
 function notify_system {
-  notify-send -t $1 $2
+  html_translated=$(echo "$2" | sed '{s/\\n/\r/g}');
+  notify-send -t $1 "$html_translated";
+  if [ $? -ne 0 ]; then
+    return 1;
+  else
+    return 0;
+  fi
 }
 
 function notify() {
@@ -35,18 +60,20 @@ function notify() {
   #   Telegram notification is available
   # second argument is notification text
   #   Time delay is not used in Telegram notification process
+  logger "INFO" "$2"
   if [ $is_telegram_available -eq 1 ]; then
-    notify_telegram $telegram_token $telegram_chat_id "$DATE_TIME_NOW\n$2";
+    result=$(notify_telegram $telegram_token $telegram_chat_id "$DATE_TIME_NOW\n$2");
+    if [ "$result" != 0 ]; then
+      notify_system $1 "$DATE_TIME_NOW\n$2";
+    fi
   else
     notify_system $1 "$DATE_TIME_NOW\n$2";
   fi
 }
 
 function backup() {
-  notify 3000 'Backuping sync folder';
   tar --exclude "node_modules" -I pigz -cf $2/$(date +'%m-%d-%Y').tar.gz $1 &>/dev/null;
-  result=$?;
-  if [ $result -ne 0 ]; then
+  if [ $? -ne 0 ]; then
     echo 1;
   else
     echo 0;
@@ -54,10 +81,8 @@ function backup() {
 }
 
 function sync() {
-  notify 5000 "Start folders sync";
   rsync -az --update --progress --exclude='node_modules' $1@$2:$3 $4 &>/dev/null;
-  result=$?;
-  if [ $result -ne 0 ]; then
+  if [ $? -ne 0 ]; then
     echo 1;
   else
     echo 0;
@@ -65,31 +90,31 @@ function sync() {
 }
 
 if [ -z "$username" ]; then
-  echo "Remote username must be specified";
+  logger "CRITICAL" "Remote username must be specified";
   exit 1;
 else
-  echo "Remote username is: '$username'";
+  logger "INFO" "Remote username is: '$username'";
 fi
 
 if [ -z "$host" ]; then
-  echo "Remote host must be specified";
+  logger "CRITICAL" "Remote host must be specified";
   exit 1;
 else
-  echo "Remote host is: '$host'";
+  logger "INFO" "Remote host is: '$host'";
 fi
 
 if [ -z "$remote_path" ]; then
-  echo "Remote path must be specified";
+  logger "CRITICAL" "Remote path must be specified";
   exit 1;
 else
-  echo "Remote path is: '$remote_path'";
+  logger "INFO" "Remote path is: '$remote_path'";
 fi
 
 if [ -z "$local_path" ]; then
   echo "Local path must be specified";
   exit 1;
 else
-  echo "Local path is: '$local_path'";
+  logger "INFO" "Local path is: '$local_path'";
 fi
 
 if [ -z "$archive_path" ]; then
@@ -97,36 +122,38 @@ if [ -z "$archive_path" ]; then
   exit 1;
 else
   archive_path=$archive_path/$(date +'%m/%d/%Y')
-  echo "Archive path is: '$archive_path'";
+  logger "INFO" "Archive path is: '$archive_path'";
   mkdir -p $archive_path;
 fi
 
 if [ -z "$compression_level" ]; then
-  echo "Compression level is not specified. Default is: $DEFAULT_COMPRESSION_LEVEL";
+  logger "INFO" "Compression level is not specified. Default is: $DEFAULT_COMPRESSION_LEVEL";
   compression_level=$DEFAULT_COMPRESSION_LEVEL;
 else
-  echo "Compression level is: '$compression_level'";
+  logger "INFO" "Compression level is: '$compression_level'";
 fi
 
 if [ ! -z "$telegram_token" ] || [ ! -z "$telegram_chat_id" ]; then
-  echo "Using Telegram notifications";
+  logger "INFO" "Using Telegram notifications";
   is_telegram_available=1;
 else
-  echo "Telegram token is not configured. Using system notifications";
+  logger "WARNING" "Telegram token is not configured. Using system notifications";
   is_telegram_available=0;
 fi
 
+notify 3000 'Backuping sync folder';
 backup_result=$(backup $local_path $archive_path);
-if [ $backup_result -ne 0 ]; then
+if [ "$backup_result" != 0 ]; then
   notify 10000 "Something went wrong while backup process";
-  exit 1
+  exit 1;
 else
   notify 5000 "Backup created successfully";
 
+  notify 5000 "Start folders sync";
   sync_result=$(sync $username $host $remote_path $local_path);
-  if [ $sync_result -ne 0 ]; then
+  if [ "$sync_result" != 0 ]; then
     notify 10000 "Directory sync FAILED";
-    exit 1
+    exit 1;
   else
     notify 5000 "Directory sync completed successfully";
   fi
